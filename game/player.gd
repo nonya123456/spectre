e58b135f4fb41a68681bddef0c140e04f47243c7
nonya123456 <nonya579@gmail.com>
2025-06-24 23:20:33 +0700

    feat: add player movement

--- game/player.gd
+++ game/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @onready var camera: Camera3D = $Camera3D
 
+@export var move_speed: float = 5.0
 @export var sensitivity: float = 0.2
 var look_input: Vector2 = Vector2.ZERO
 var pitch: float = 0.0
@@ -13,6 +14,27 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _physics_process(delta: float) -> void:
+    var input_dir: Vector3 = Vector3.ZERO
+
+    if Input.is_action_pressed("move_forward"):
+        input_dir.z -= 1
+
+    if Input.is_action_pressed("move_back"):
+        input_dir.z += 1
+
+    if Input.is_action_pressed("move_left"):
+        input_dir.x -= 1
+
+    if Input.is_action_pressed("move_right"):
+        input_dir.x += 1
+
+    input_dir = input_dir.normalized()
+
+    var move_dir: Vector3 = global_basis.z * input_dir.z + global_basis.x * input_dir.x
+    move_dir = move_dir.normalized() * move_speed
+
+    velocity.x = move_dir.x
+    velocity.z = move_dir.z
     velocity += Vector3.DOWN * 9.8 * delta
 
     move_and_slide()
