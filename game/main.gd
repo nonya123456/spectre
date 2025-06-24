9fe05572b32f171d26bce82bce72a6355737cf2f
nonya123456 <nonya579@gmail.com>
2025-06-25 00:07:29 +0700

    feat: lower resolution

--- game/main.gd
+++ game/main.gd
@@ -7,6 +7,6 @@ func _ready() -> void:
 
 func _notification(what: int) -> void:
     if what == NOTIFICATION_APPLICATION_FOCUS_IN:
-        Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     elif what == NOTIFICATION_APPLICATION_FOCUS_OUT:
         Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
