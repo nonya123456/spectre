ab489bd207adecc0f67e82849a52311c02fd9f9d
nonya123456 <nonya579@gmail.com>
2025-06-24 23:00:43 +0700

    feat: show and hide mouse

--- game/main.gd
+++ game/main.gd
@@ -2,5 +2,11 @@ extends Node
 
 
 func _ready() -> void:
-    Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
     print("Hello from main.gd")
+
+
+func _notification(what: int) -> void:
+    if what == NOTIFICATION_APPLICATION_FOCUS_IN:
+        Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+    elif what == NOTIFICATION_APPLICATION_FOCUS_OUT:
+        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
