a211c2b52804fe851e13ad30247d994b7486ef94
nonya123456 <nonya579@gmail.com>
2025-06-29 00:26:47 +0700

    feat: add view model

---
+++ game/player/player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+@onready var camera: Camera3D = $Camera3D
+@onready var view_model: Node3D = $Camera3D/SubViewportContainer/SubViewport/ViewModel
+@onready var spot_light: SpotLight3D = $SpotLight3D
+
+@export var move_speed: float = 5.0
+@export var sensitivity: float = 0.2
+var look_input: Vector2 = Vector2.ZERO
+var pitch: float = 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_input = event.screen_relative
+
+
+func _physics_process(delta: float) -> void:
+	var input_dir: Vector3 = Vector3.ZERO
+
+	if Input.is_action_pressed("move_forward"):
+		input_dir.z -= 1
+
+	if Input.is_action_pressed("move_back"):
+		input_dir.z += 1
+
+	if Input.is_action_pressed("move_left"):
+		input_dir.x -= 1
+
+	if Input.is_action_pressed("move_right"):
+		input_dir.x += 1
+
+	input_dir = input_dir.normalized()
+
+	var move_dir: Vector3 = global_basis.z * input_dir.z + global_basis.x * input_dir.x
+	move_dir = move_dir.normalized() * move_speed
+
+	velocity.x = move_dir.x
+	velocity.z = move_dir.z
+	velocity += Vector3.DOWN * 9.8 * delta
+
+	move_and_slide()
+
+	_handle_look()
+
+	_update_view_model()
+
+
+func _handle_look() -> void:
+	pitch -= look_input.y * sensitivity
+	pitch = clamp(pitch, -89, 89)
+	camera.rotation_degrees.x = pitch
+	spot_light.rotation_degrees.x = pitch
+
+	var horizontal: float = - look_input.x * sensitivity
+	var horizontal_rad: float = deg_to_rad(horizontal)
+	rotate_y(horizontal_rad)
+
+	look_input = Vector2.ZERO
+
+
+func _update_view_model() -> void:
+	view_model.global_position = camera.global_position
+	view_model.global_rotation_degrees = camera.global_rotation_degrees
