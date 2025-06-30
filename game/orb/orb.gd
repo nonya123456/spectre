e16c07ac6a5720c709b25c01858602ea903d9580
nonya123456 <nonya579@gmail.com>
2025-06-30 16:10:02 +0700

    feat: add orb

---
+++ game/orb/orb.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+@onready var model: Node3D = $OrbModel
+
+@export var speed: float = 1.0
+
+
+func _process(delta: float) -> void:
+    model.rotate(Vector3(1, 1, 1).normalized(), delta * speed)
