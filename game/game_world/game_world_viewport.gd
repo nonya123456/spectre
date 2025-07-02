057bb29cf73586d90d6818a81c3a0d9aaecebc6b
nonya123456 <nonya579@gmail.com>
2025-07-02 18:37:01 +0700

    feat: add screen shake

--- game/game_world/game_world_viewport.gd
+++ game/game_world/game_world_viewport.gd
@@ -1,13 +1,17 @@
 extends SubViewportContainer
 
 var force: float = 1.0
-var target_force: float = 1.0
 var force_change_speed: float = 10.0
+var is_zoomed: bool = false
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("toggle_zoom"):
-		target_force = 1.0 if target_force == 1.5 else 1.5
+		is_zoomed = !is_zoomed
 
+	var target_force: float = 1.5 if is_zoomed else 1.0
 	force = lerp(force, target_force, delta * force_change_speed)
 	material.set_shader_parameter("force", force)
+
+	var shake_intensity: float = 0.005 if is_zoomed else 0.0
+	material.set_shader_parameter("shake_intensity", shake_intensity)
