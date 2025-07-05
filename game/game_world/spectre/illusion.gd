38a23815419f8fa181b09d39c0b287d6567df834
nonya123456 <nonya579@gmail.com>
2025-07-05 11:12:49 +0700

    feat: change sight range to xz

--- game/game_world/spectre/illusion.gd
+++ game/game_world/spectre/illusion.gd
@@ -10,7 +10,7 @@ var found_timer: float = 0.75
 @onready var marker: Marker3D = $Marker3D
 @onready var spectre_model: SpectreModel = $SpectreModel
 
-@export var sight_range: float = 7.5
+@export var sight_range: float = 6.0
 @export_flags_3d_physics var collision_mask: int
 
 
@@ -28,14 +28,10 @@ func _physics_process(_delta: float) -> void:
 		return
 
 	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-
-	var raycast_direction: Vector3 = (target.global_position - marker.global_position).normalized()
-	var raycast_target: Vector3 = marker.global_position + sight_range * raycast_direction
-	var query := PhysicsRayQueryParameters3D.create(marker.global_position, raycast_target, collision_mask, [self])
-
+	var query := PhysicsRayQueryParameters3D.create(marker.global_position, target.global_position, collision_mask, [self])
 	var result = space_state.intersect_ray(query)
 
-	if result.has("collider") and result["collider"] == target:
+	if result.has("collider") and result["collider"] == target and disp.length() <= sight_range:
 		handle_target_entered_sight()
 
 
--- game/game_world/spectre/spectre.gd
+++ game/game_world/spectre/spectre.gd
@@ -10,7 +10,7 @@ var target_in_sight: bool = false
 
 @onready var marker: Marker3D = $Marker3D
 
-@export var sight_range: float = 7.5
+@export var sight_range: float = 6.0
 @export_flags_3d_physics var collision_mask: int
 
 
@@ -25,17 +25,13 @@ func _physics_process(_delta: float) -> void:
 	look_at(global_position - direction, Vector3.UP)
 
 	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-
-	var raycast_direction: Vector3 = (target.global_position - marker.global_position).normalized()
-	var raycast_target: Vector3 = marker.global_position + sight_range * raycast_direction
-	var query := PhysicsRayQueryParameters3D.create(marker.global_position, raycast_target, collision_mask, [self])
-
+	var query := PhysicsRayQueryParameters3D.create(marker.global_position, target.global_position, collision_mask, [self])
 	var result = space_state.intersect_ray(query)
 
-	if (result.has("collider") and result["collider"] == target) and !target_in_sight:
+	if !target_in_sight and (result.has("collider") and result["collider"] == target and disp.length() <= sight_range):
 		target_in_sight = true
 		target_found.emit(marker.global_position)
 
-	if (!result.has("collider") or result["collider"] != target) and target_in_sight:
+	if target_in_sight and (!result.has("collider") or result["collider"] != target or disp.length() > sight_range):
 		target_in_sight = false
 		target_lost.emit()
