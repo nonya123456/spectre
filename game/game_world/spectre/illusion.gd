6f5bb9ac78ee2191f2917c78493360d829053de6
nonya123456 <nonya579@gmail.com>
2025-07-09 13:54:18 +0700

    fix: teleport sound not finished

--- game/game_world/spectre/illusion.gd
+++ game/game_world/spectre/illusion.gd
@@ -13,11 +13,11 @@ var found_timer: float = 0.25
 
 @onready var marker: Marker3D = $Marker3D
 @onready var spectre_model: SpectreModel = $SpectreModel
-@onready var teleport_player: AudioStreamPlayer3D = $TeleportPlayer
 
 @export var inactive_range: float = 20.0
 @export var sight_range: float = 4.0
 @export_flags_3d_physics var collision_mask: int
+@export var teleport_player_scene: PackedScene = preload("res://game/game_world/spectre/teleport_player.tscn")
 
 
 func _physics_process(_delta: float) -> void:
@@ -43,7 +43,16 @@ func _physics_process(_delta: float) -> void:
 
 	if result.has("collider") and result["collider"] == target and disp.length() <= sight_range:
 		_deactivate()
-		teleport_player.play()
+		_play_teleport_sound()
+
+
+func _play_teleport_sound() -> void:
+	var teleport_player: AudioStreamPlayer3D = teleport_player_scene.instantiate()
+	get_parent().add_child(teleport_player)
+	teleport_player.global_position = global_position
+	teleport_player.finished.connect(func() -> void:
+		teleport_player.queue_free()
+	)
 
 
 func _process(delta: float) -> void:
