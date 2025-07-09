60eeff4866bebd759b31aec6d51fb91e02f7b44d
nonya123456 <nonya579@gmail.com>
2025-07-09 20:15:32 +0700

    feat: add master volume settings

--- game/main_menu/main_menu.gd
+++ game/main_menu/main_menu.gd
@@ -13,13 +13,18 @@ var has_quit_button_pressed: bool
 @onready var credits: Node = $SubViewportContainer/SubViewport/Credits
 @onready var credits_text: RichTextLabel = $SubViewportContainer/SubViewport/Credits/ScrollContainer/VBoxContainer/RichTextLabel
 @onready var settings: Node = $SubViewportContainer/SubViewport/Settings
+@onready var master_volume_h_slider: HSlider = $SubViewportContainer/SubViewport/Settings/VBoxContainer/MasterVolume/HSlider
 
 @export var button_pressed_player_scene: PackedScene = preload("res://game/shared/sounds/button_pressed_player.tscn")
+
+var master_bus_index: int
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	credits_text.text = "Developed using Godot Engine\n\nLicense for Godot Engine\n\n" + Engine.get_license_text()
+	master_bus_index = AudioServer.get_bus_index("Master")
+	master_volume_h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus_index))
 
 
 func _on_play_button_pressed() -> void:
@@ -69,3 +74,7 @@ func _play_button_pressed_sound() -> void:
 	button_pressed_player.finished.connect(func() -> void:
 		button_pressed_player.queue_free()
 	)
+
+
+func _on_master_volume_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
